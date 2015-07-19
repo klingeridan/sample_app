faba800b24089e0060028c60fa4df85cccd50d27
Idan Klinger <idan.k@fiverr.com>
2015-07-19 14:37:32 +0300

    Switched from form_for to form_tag

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,10 @@ RSpec::Matchers.define :have_error_message do |message|
 		page.should have_selector('div.alert.alert-error', text: 'Invalid')
 	end
 end
+
+def create_valid_user
+	fill_in "Name", 		with: "Example User"
+	fill_in "Email", 		with: "user@example.com"
+	fill_in "Password", 	with: "foobar"
+	fill_in "Confirmation", with: "foobar"
+end
