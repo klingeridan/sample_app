69422823d0ab08afcac7b09b48a4ea6b87a88f31
Idan Klinger <idan.k@fiverr.com>
2015-07-21 15:13:13 +0300

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,19 +25,13 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
   it { should_not be_admin }
 
   describe "'admin' attribute should not be accessible" do
     specify { expect { User.new(name:"a", email: "a@gmail.com", password:"foobar", password_confirmation:"foobar", admin: true) }.to raise_error }
-
-    # admin_param = { :admin => true }
-    # params = ActionController::Parameters.new(admin_param)
-    # expect { @user.update_attributes(params) }.should raise_error
-
-    # before { @user.admin = true }
-    # it { should_not be_valid }
   end
 
   describe "with admin attribute set to 'true'" do
@@ -142,6 +136,40 @@ describe User do
     describe "remember token" do
       before { @user.save }
       its(:remember_token) { should_not be_blank }
+    end
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status" do
+      let!(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
     end
   end
 end
